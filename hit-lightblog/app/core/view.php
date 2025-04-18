c5d934071fc2e8ad920dabe393cb1952e4047f36
Kjell Heinsen <ich@kjell-heinsen.de>
2025-04-18 12:52:40 +0200

    core/view

---
+++ hit-lightblog/app/core/view.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace hitlightblog\app\core;
+
+class view
+{
+    function __construct()
+    {
+
+    }
+
+
+    public function render(string $path, array $data) {
+        $path = DOCROOT."/app/views/$path.php";
+        if(file_exists($path))
+        {
+            require $path;
+        }
+        else
+        {
+         file_put_contents($path, 'Dieses View('.$path.') ist leer und automatisch generiert.</br>');
+         require $path;
+         unlink($path);
+        }
+    }
+
+
+}
