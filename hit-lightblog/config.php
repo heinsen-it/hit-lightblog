3da511d75cbea873f485ae9a592305ee33ad4e37
Kjell Heinsen <ich@kjell-heinsen.de>
2025-04-18 15:24:30 +0200

    Config Update

--- hit-lightblog/config.php
+++ hit-lightblog/config.php
@@ -3,4 +3,5 @@ date_default_timezone_set('Europe/Berlin');
 define('DIR','https://example.com/');
 define('DOCROOT', __DIR__);
 define('DefaultC','start');
+define('SESSION_PREFIX', 'hitbloglight_');
 require_once 'sql_credits.php';
